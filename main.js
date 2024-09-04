3e5806a23df0789ca3a8422ecfa609a05f843d79
Abir Hossen <abir.cse4@gmail.com>
2024-09-04 10:44:09 +0600

    First Commit

---
+++ main.js
@@ -0,0 +1,34 @@
+let score = 75;
+let grade;
+if (score >= 90) {
+ grade = "A";
+} else if (score >= 80) {
+ grade = "B";
+}
+console.log(grade);
+
+
+
+const time = new Date().getHours();
+let greeting;
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+document.getElementById("demo").innerHTML = greeting;
+
+
+
+console.log(33);
+
+
+
+var weight = 38;
+var price = 40;
+var year = 2000;
+var money = 5000;
+
+console.log(money);
---
+++ veriable.js
@@ -0,0 +1,47 @@
+var weight = 38;
+var price = 40;
+var year = 2000;
+var money = 5000;
+
+console.log(money);
+
+
+// String type variable
+var name = "Abir Hossen";
+var address = 'Daulotpur Kushtia';
+var country = 'Bangladesh';
+
+console.log(name);
+
+var isRich = true;
+console.log(isRich);
+
+console.log(typeof country);
+
+var orange = 20;
+var chocolate = 0.5;
+var apple = parseInt('100');
+
+console.log(orange + apple);
+
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total.toFixed(2));
+
+
+var price = 35;
+price += 10;
+console.log(price);
+
+var price2 = 3000;
+price2 = price2 - 1000;
+console.log(price2);
+
+
+
+
+
+
+
