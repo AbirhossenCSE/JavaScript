e1b9444ffa62dad1646eeade2fac714b7d775914
Abir Hossen <abir.cse4@gmail.com>
2024-09-05 00:02:17 +0600

    JS

---
+++ loops/loops.js
@@ -0,0 +1,38 @@
+// veriable
+const amount = 200;
+
+// codition
+if (amount > 100) {
+    
+} else {
+    
+}
+
+
+
+
+// Array
+// for loop
+
+const numbers = [12, 32, 45, 63, 21, 72];
+
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+    
+}
+
+
+
+const fruits = ['banana', 'mango', 'apple', 'lichi']
+
+for(const fruit of fruits){
+    console.log(fruit);
+    
+}
+
+
+
+
+
