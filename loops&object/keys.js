5012602ea131c8bef47ca9a1745d475e42fe341c
Abir Hossen <abir.cse4@gmail.com>
2024-09-07 13:53:06 +0600

    keys & values

---
+++ loops&object/keys.js
@@ -0,0 +1,15 @@
+const computer = {
+    brand: 'asus',
+    price: 70000,
+    pricessor: 'intel',
+    hdd: '512gb'
+}
+
+
+// keys
+const keys = Object.keys(computer);
+console.log(keys);
+
+// velues
+const velues = Object.values(computer);
+console.log(velues);
