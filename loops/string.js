56e07eceaefbdd73c58e372a81eb8a3e8b16f8f0
Abir Hossen <abir.cse4@gmail.com>
2024-09-07 10:06:21 +0600

    JS

---
+++ loops/string.js
@@ -0,0 +1,29 @@
+const country = 'Bangladesh';
+const division = "Khulna";
+const district = `Kushtia`;
+const thana = new String('Daulotpur');
+
+console.log(typeof country);
+console.log(typeof division);
+console.log(typeof district);
+console.log(typeof thana);
+
+const num = [54, 23, 35, 75, 93];
+console.log(num);
+
+num[1] = 11;
+console.log(num);
+
+const capital = 'Dhaka';
+console.log(capital);
+console.log(capital.length);
+console.log(capital[1]);
+
+
+
+
+
+
+
+
+
