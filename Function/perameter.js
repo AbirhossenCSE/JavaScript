177d8a63937553eb622860e219086fc84d10ce10
Abir Hossen <abir.cse4@gmail.com>
2024-09-09 20:26:55 +0600

    perameter

---
+++ Function/perameter.js
@@ -0,0 +1,33 @@
+function vatKhabo(){
+    console.log('hat dhuye aso');
+    console.log('boso');
+    console.log('khabar new');
+    console.log('khawa suru koro');
+   
+}
+// vatKhabo();
+
+
+// Square function
+function squre(number) {
+    console.log('Value of the perameter', number);
+    const borgo = number * number;
+    console.log('Square of the given number is: ', borgo);
+    
+    
+}
+squre(5);
+console.log('-----------');
+squre(222);
+console.log('-----------');
+squre(403);
+console.log('-----------');
+
+
+// Add function
+function add(num1, num2) {
+    const sum = num1 + num2;
+    console.log('Sum of these two number: ', sum);
+    
+}
+add(45, 5);
