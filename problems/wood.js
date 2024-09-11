d6d20f48a716e781374ce7a48cff05a2432b0ac2
Abir Hossen <abir.cse4@gmail.com>
2024-09-12 01:13:04 +0600

    wood hisab

---
+++ problems/wood.js
@@ -0,0 +1,16 @@
+function woodQuantity(chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 50;
+
+    const chairTotalwood = chairQuantity * perChairWood;
+    const tableTotalWood = tableQuantity * perTableWood;
+    const bedTotalWood = bedQuantity * perBedWood;
+
+    const totalWood = chairTotalwood + tableTotalWood + bedTotalWood;
+    return totalWood;
+}
+
+const wood = woodQuantity(1, 1, 1);
+console.log(wood);
+
