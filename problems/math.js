e44132b4c44c7c8a6592a7370ab34ef38b362953
Abir Hossen <abir.cse4@gmail.com>
2024-09-11 23:11:46 +0600

    math

---
+++ problems/math.js
@@ -0,0 +1,29 @@
+const min = Math.min(45, 46, 546, 46, 546, -55);
+console.log(min);
+const max = Math.max(45, 46, 546, 46, 546, -55);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(5-50));
+console.log(Math.round(2.11));
+console.log(Math.round(2.66));
+console.log(Math.floor(2.33));
+console.log(Math.floor(2.99));
+console.log(Math.ceil(2.11));
+console.log(Math.ceil(2.77));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
+
+
+
+
+
+
+
+
+
+
