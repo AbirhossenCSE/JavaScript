f8c569d74bbf5da088cad78996389849f875e513
Abir Hossen <abir.cse4@gmail.com>
2024-09-23 22:23:32 +0600

    transaction

---
+++ payooo/newpayoo/features.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-show-add-money').addEventListener('click', function(){
+    shoeSectionById('add-money-form')
+    
+})
+document.getElementById('btn-show-cash-out').addEventListener('click', function(){
+    shoeSectionById('cash-out-form')
+    
+})
+document.getElementById('btn-show-transaaction').addEventListener('click', function(){
+    shoeSectionById('transaction-section')
+    
+})
---
+++ payooo/newpayoo/utilities.js
@@ -0,0 +1,27 @@
+
+
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+
+    // console.log(id, inputValue, inputNumber);
+    return inputNumber;
+    
+}
+
+function getTextFieldValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    return textNumber;
+}
+
+
+function shoeSectionById(id){
+    // hide all section
+    document.getElementById('add-money-form').classList.add('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+
+    // show the section with the provide id as perameter
+    document.getElementById(id).classList.remove('hidden')
+}
