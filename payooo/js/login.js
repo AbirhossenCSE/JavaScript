91cce45a4c6ffd28376bb967911f9b2a022d21d4
Abir Hossen <abir.cse4@gmail.com>
2024-09-23 16:25:09 +0600

    cash

--- payooo/js/login.js
+++ payooo/js/login.js
@@ -5,9 +5,20 @@ document.getElementById('button-login').addEventListener('click', function(event
     event.preventDefault();
     console.log('Login button clicked');
 
-    // S-2: get the phone number
+    // S-2: get the phone number & pin numbre
     const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    console.log('Pin', pinNumber);
     console.log(phoneNumber);
     
-    
+    // Validate phone number and Pin But this is not  right way
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('Loged in');
+        window.location.href = '/home.html'
+        
+    } else {
+        alert('Number or Pin Invalid');
+        
+    }
 })
