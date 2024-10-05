e35a00aa5d15be18ef26503f55ccf68597444bb2
Abir Hossen <abir.cse4@gmail.com>
2024-10-05 21:38:21 +0600

    dup

--- payooo/newpayoo/addmoneyextra.js
+++ payooo/newpayoo/addmoneyextra.js
@@ -159,3 +159,36 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     }
     
 })
+
+
+// Duplicate
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+    // console.log(addMoney, pinNumber);
+
+    if (isNaN(addMoney) || addMoney <= 0) {
+        alert('Failed! Try Again');
+        return;
+    }
+
+    if (pinNumber === 1234) {
+        const balance = getTextFieldValueById('account-balance');
+        const newBalance = balance + addMoney;
+
+        // new balance
+        document.getElementById('account-balance').innerText = newBalance;
+
+        // add to transaction history
+        const p = document.createElement('p');
+        p.innerText = `Added: ${addMoney} Tk. Balance: ${newBalance}`;
+        // should be a common function
+        document.getElementById('transaction-container').appendChild(p);
+        
+    } else {
+        alert('Failed! Try Again')
+    }
+    
+})
