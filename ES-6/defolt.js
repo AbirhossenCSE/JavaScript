e2ce8decd2aa6f0a106743c4dee67903f79136d6
Abir Hossen <abir.cse4@gmail.com>
2024-10-07 11:21:32 +0600

    defolt value

---
+++ ES-6/defolt.js
@@ -0,0 +1,10 @@
+
+// defolt value
+function add (num1 = 0, num2 = 0){
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    
+    return result;
+}
+const sum = add(10, 5);
+// const sum = add(10);
