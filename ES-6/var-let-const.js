a457c3a740c9a082b9d9039808850d2130c2b67a
Abir Hossen <abir.cse4@gmail.com>
2024-10-07 11:05:06 +0600

    ES

---
+++ ES-6/var-let-const.js
@@ -0,0 +1,31 @@
+const money = 10;
+money3 = money +2;
+console.log(money3);
+
+
+let money2 = 10;
+money2 = 5;
+console.log(money2);
+
+const numbers = [2, 54, 65, 85, 25];
+numbers[1] = 34;
+numbers.push(45, 8, 3);
+console.log(numbers);
+
+
+const student = {
+    name: 'abir hossen',
+    age: 22
+}
+
+student.name = 'Alvi';
+console.log(student);
+
+
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+    
+}
+console.log(sum);
